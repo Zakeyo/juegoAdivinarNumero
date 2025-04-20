aeaeb37e0725192aaf2be7f5ab6aea71c808756b
Zakeyo <manuelc.dev@gmail.com>
2025-04-20 15:40:18 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace juegoAdivinarNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int numberRandom = random.Next(0, 100);
+
+            Console.WriteLine("Vamos a jugar el juego de adivinar el número.");
+            Console.WriteLine("Intenta adivinar el número que elegí del 1 al 100");
+
+            string input;
+            int number = -1;
+
+            while (number != numberRandom)
+            {
+                input = Console.ReadLine();
+
+                if (!int.TryParse(input, out number) || number < 0 || number > 100)
+                {
+                    Console.WriteLine("Has introducido un dato inválido, debe ser un número entre el 1 y 100");
+                }
+                else if (number > numberRandom)
+                {
+                    Console.WriteLine("↓↓↓ El valor es menor ↓↓↓");
+                }
+                else if (number < numberRandom)
+                {
+                    Console.WriteLine("↑↑↑ El valor es mayor ↑↑↑");
+                }
+            }
+
+            Console.WriteLine("FELICIDADES, HAS GANADO EL JUEGO!");
+        }
+    }
+}
